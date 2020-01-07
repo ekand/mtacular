44d71a50edf7a7c9d2699d3bf8d21e99b9d01a2c
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2020-01-07 15:19:47 -0600

    create process_mta_data.py

---
+++ notebooks/code/process_mta_data.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+from pathlib import Path
+
+PROJECT_DIR = str(Path(__file__).resolve().parents[2])
+
+
+def add_datetime(df):
+    """
+    adds a datetime column to the mta dataframes
+    :param df:
+    :return: a dataframe with a new datetime column, generated from
+
+    """
+    
