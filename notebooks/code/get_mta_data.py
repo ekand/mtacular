cb99ebb9e2f1f69b6b5dedf4e6fff226a7790afb
Erik Kristofer Anderson <erik.kristofer.anderson@gmail.com>
2020-01-07 12:32:34 -0600

    add get_mta_data.py
    The purpose of this file is for its functions to get called from jupyter notebooks to download data in a systematic way to mtacular/data/raw/

--- notebooks/code/get_mta_data.py
+++ notebooks/code/get_mta_data.py
@@ -4,63 +4,47 @@
 import pandas as pd
 
 # Get data from MTA website according to variables **years** and **months**.
-# Generates a wget list in **wget_file** and wgets to **../data**
+# saves data to ../../data/raw/
 
-
-
-file_url = 'http://johndoe.com/download.zip'
-
-# file_name = wget.download(file_url)
-import urllib
 
 import datetime
-import csv
 from pathlib import Path
 
 
-wget_file = '../temp/wget_file.txt'
-
-
 def get_data(years, months):
-    pass
+    filenames = generate_filenames(years, months)
+    for filename in filenames:
+        download_one_file(filename)
+        print(f"downloaded {filename}")
+
+def generate_filenames(years, months):
+    filenames = []
+    final = datetime.date(2019, 12, 28)
+    file_delta = datetime.timedelta(days=7)
+    current = final
+    while current.year >= min(years):
+        if current.year in years and current.month in months:
+            filenames.append(current.strftime('turnstile_%y%m%d.txt'))
+        current -= file_delta
+    return filenames
 
 
 def download_one_file(filename):
+    import urllib.request
     base_url = 'http://web.mta.info/developers/data/nyct/turnstile/'
     url = base_url + filename
-    # target_date = datetime.date(2019, 12, 28)
-
-    file_path = raw_data_directory + file_name
-    # test_url_getter = urllib.URLopener
-    # test_url_getter.retrieve("url", file_path)
-    import urllib.request
-
-    print('Beginning file download with urllib2...')
-
-    # url = 'http://i3.ytimg.com/vi/J---aiyznGQ/mqdefault.jpg'
-    urllib.request.urlretrieve(url, '../../data/raw/cat.jpg')
-
-
-    return None
-
-
-# def generate file_list(years, months):
-#     pass
+    base_data_path = "../../data/raw/"
+    file_path = base_data_path + filename
+    urllib.request.urlretrieve(url, file_path)
 
 
 if __name__ == "__main__":
-    # years = [2018, 2019]
-    # months = [3, 4, 5]
-    # get_data(years, months)
-    #
-    # final = datetime.date(2019.12.28)
-    # file_delta = datetime.timedelta(days=7)
-    # url = 'http://web.mta.info/developers/data/nyct/turnstile/'
-    #
-    # file_list = []
+    years_input = [2018, 2019]
+    years_input = [2019]
+    months_input = [3, 4]
+
     print(f"running {str(Path(__file__))}")
-    file_name = "turnstile_200104.txt"
-    download_one_file(file_name)
-    print("tests complete")
+    get_data(years_input, months_input)
+    print("script complete")
 
 
