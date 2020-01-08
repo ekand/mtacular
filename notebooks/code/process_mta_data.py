a7783ae2d675567b9bd578ca804b9a7bc370aaf3
Eli T. Drumm <etd@dte.li>
2020-01-08 14:17:22 -0600

    added ins/outs function to process_mta_data

--- notebooks/code/process_mta_data.py
+++ notebooks/code/process_mta_data.py
@@ -27,3 +27,13 @@ def clean_col_names(df):
     before = 'EXITS                                                               '
     df.rename(columns={before: 'EXITS'}, inplace=True)
     return df
+
+def add_ins_outs_to_df(df):
+    """
+    determines the difference of each row's ENTRIES count from the previous row's. It won't diff between different turnstiles, but make sure to run this before any sorting
+
+    :param df: dataframe to manipulate (in place)
+    """
+    mask = df.duplicated(['CA', 'UNIT', 'SCP', 'STATION'])
+    df['INS'] = np.where(mask, df['ENTRIES'] - df['ENTRIES'].shift(1), np.nan)
+    df['OUTS'] = np.where(mask, df['EXITS'] - df['EXITS'].shift(1), np.nan)
